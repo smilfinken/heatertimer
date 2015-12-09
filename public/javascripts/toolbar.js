bed66151f43f7b5ebb515ee80fefb75824a04209
smilfinken <johan.x.lindquist@gmail.com>
2015-12-09 16:03:00 +0100

    finished toolbar edit and delete timer setting functionality

--- public/javascripts/toolbar.js
+++ public/javascripts/toolbar.js
@@ -1,21 +1,38 @@
 (function (window) {
-	function disableButton(element) {
-		var button = document.getElementById(element);
-        classie.add(button, "disabled");
-	}
-
 	function enableButton(element) {
 		var button = document.getElementById(element);
-        classie.remove(button, "disabled");
+        if (!!button && classie.has(button, "disabled")) {
+            classie.remove(button, "disabled");
+        }
+	}
+
+	function disableButton(element) {
+		var button = document.getElementById(element);
+        if (!!button && !classie.has(button, "disabled")) {
+            classie.add(button, "disabled");
+        }
 	}
 
     function connectButton(element, target) {
 		var button = document.getElementById(element);
-		button.setAttribute("form", target);
+		if (!!button) {
+            button.setAttribute("form", target);
+            enableButton(button.id);
+		}
+    }
+
+    function disconnectButton(element, target) {
+		var button = document.getElementById(element);
+		if (!!button) {
+            button.removeAttribute("form");
+            disableButton(button.id);
+		}
     }
 
 	window.toolbar = {
 		enableButton: enableButton,
-		connectButton: connectButton
+		disableButton: disableButton,
+		connectButton: connectButton,
+		disconnectButton: disconnectButton
 	};
 })(window);
--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -25,14 +25,45 @@
         }
     }
 
+    function highlightSingleRow(id) {
+        var row = document.getElementById(id);
+        if (!!row) {
+            var rows = row.parentNode.getElementsByTagName("tr");
+            for (var i = 0; i < rows.length; i++) {
+                if (rows[i].id === id) {
+                    if (!classie.has(rows[i], "highlighted")) {
+                        classie.add(rows[i], "highlighted");
+                    }
+                } else {
+                    if (classie.has(rows[i], "highlighted")) {
+                        classie.remove(rows[i], "highlighted");
+                    }
+                }
+            }
+            return true;
+        }
+    }
+
     function highlightRow(id) {
         var row = document.getElementById(id);
-        classie.toggle(row, "highlighted");
+        if (!!row) {
+            classie.toggle(row, "highlighted");
+        }
+    }
+
+    function setValue(target, attribute, value) {
+        var element = document.getElementById(target);
+        if (!!element) {
+            element.setAttribute(attribute, value);
+            return true;
+        }
     }
 
 	window.utils = {
 		incrementValue: incrementValue,
 		decrementValue: decrementValue,
-		highlightRow: highlightRow
+		highlightSingleRow: highlightSingleRow,
+		highlightRow: highlightRow,
+		setValue: setValue
 	};
 })(window);
