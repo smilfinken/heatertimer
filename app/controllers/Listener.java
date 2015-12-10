7e88c753bee8b762904db739fdb7ee62b86958ed
smilfinken <johan@smilfinken.net>
2015-12-11 00:02:40 +0100

    started on relay setting logic - fix for previous

--- app/controllers/Listener.java
+++ app/controllers/Listener.java
@@ -2,6 +2,7 @@ package controllers;
 
 import models.SensorReading;
 
+import java.util.ArrayList;
 import java.util.logging.Logger;
 import java.util.logging.Level;
 
@@ -12,6 +13,7 @@ import play.mvc.Result;
 import play.mvc.Controller;
 import play.db.jpa.Transactional;
 import play.db.jpa.JPA;
+import javax.persistence.*;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -62,9 +64,9 @@ public class Listener extends Controller {
         boolean[] result = { false, true };
 
         try {
-            Double latestTemperature;
-            TypedQuery<Double> query = JPA.em().createQuery("SELECT sr.temperature FROM SensorReadings AS sr ORDER BY sr.timestamp DESC");
-            latestTemperature = query.setMaxResults(1).getResultList()[0];
+            Double latestReading;
+            TypedQuery<Double> query = JPA.em().createQuery("SELECT sr.temperature FROM SensorReadings AS sr ORDER BY sr.timestamp DESC", Double.class);
+            latestReading = query.setMaxResults(1).getResultList().get(0);
         } catch (Exception e) {
             LOGGER.log(Level.SEVERE, "Exception occurred when getting latest temperature reading", e);
         }
