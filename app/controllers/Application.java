f0bdcd8f225cfc28e11bf7a0bca244b30027adc3
smilfinken <johan.x.lindquist@gmail.com>
2015-12-29 23:26:19 +0100

    added values to overview and updated layout

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -40,30 +40,84 @@ public class Application extends Controller {
     }
 
     @Transactional
-    private Double getLastWeekAverageTemperature() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.WEEK_OF_YEAR, -1);
-        Date startTime = calendar.getTime();
+    private Double getAverageHumidity(Date startTime) {
+        Double result = 0.0;
 
-        return getAverageTemperature(startTime);
+        TypedQuery<Double> temperatureQuery = JPA.em().createQuery("SELECT AVG(sr.humidity) FROM SensorReading sr WHERE sr.timestamp > :starttime", Double.class);
+        temperatureQuery.setParameter("starttime", startTime);
+        try {
+            result = (Double) temperatureQuery.getSingleResult();
+        } catch (Exception e) {
+            LOGGER.warning(String.format("Application.index(): error getting average humidity: %s", e.toString()));
+        }
+
+        return result;
+    }
+
+    @Transactional
+    private Double getAveragePressure(Date startTime) {
+        Double result = 0.0;
+
+        TypedQuery<Double> temperatureQuery = JPA.em().createQuery("SELECT AVG(sr.pressure) FROM SensorReading sr WHERE sr.timestamp > :starttime", Double.class);
+        temperatureQuery.setParameter("starttime", startTime);
+        try {
+            result = (Double) temperatureQuery.getSingleResult();
+        } catch (Exception e) {
+            LOGGER.warning(String.format("Application.index(): error getting average atmospheric pressure: %s", e.toString()));
+        }
+
+        return result;
+    }
+
+    private Date getLastTimeUnit(int timeUnit) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.add(timeUnit, -1);
+        return calendar.getTime();
+    }
+
+    @Transactional
+    private Double getLastWeekAverageTemperature() {
+        return getAverageTemperature(getLastTimeUnit(Calendar.WEEK_OF_YEAR));
     }
 
     @Transactional
     private Double getLastDayAverageTemperature() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.DAY_OF_YEAR, -1);
-        Date startTime = calendar.getTime();
-
-        return getAverageTemperature(startTime);
+        return getAverageTemperature(getLastTimeUnit(Calendar.DAY_OF_YEAR));
     }
 
     @Transactional
     private Double getLastHourAverageTemperature() {
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.HOUR, -1);
-        Date startTime = calendar.getTime();
+        return getAverageTemperature(getLastTimeUnit(Calendar.HOUR_OF_DAY));
+    }
 
-        return getAverageTemperature(startTime);
+    @Transactional
+    private Double getLastWeekAverageHumidity() {
+        return getAverageHumidity(getLastTimeUnit(Calendar.WEEK_OF_YEAR));
+    }
+
+    @Transactional
+    private Double getLastDayAverageHumidity() {
+        return getAverageHumidity(getLastTimeUnit(Calendar.DAY_OF_YEAR));
+    }
+
+    @Transactional
+    private Double getLastHourAverageHumidity() {
+        return getAverageHumidity(getLastTimeUnit(Calendar.HOUR_OF_DAY));
+    }
+
+    @Transactional
+    private Double getLastWeekAveragePressure() {
+        return getAveragePressure(getLastTimeUnit(Calendar.WEEK_OF_YEAR));
+    }
+
+    @Transactional
+    private Double getLastDayAveragePressure() {
+        return getAveragePressure(getLastTimeUnit(Calendar.DAY_OF_YEAR));
+    }
+
+    @Transactional
+    private Double getLastHourAveragePressure() {
+        return getAveragePressure(getLastTimeUnit(Calendar.HOUR_OF_DAY));
     }
 
     @Transactional
@@ -72,7 +126,14 @@ public class Application extends Controller {
         Double lastDayAverageTemperature = getLastDayAverageTemperature();
         Double lastWeekAverageTemperature = getLastWeekAverageTemperature();
 
+        Double lastHourAverageHumidity = getLastHourAverageHumidity();
+        Double lastDayAverageHumidity = getLastDayAverageHumidity();
+        Double lastWeekAverageHumidity = getLastWeekAverageHumidity();
 
-        return ok(index.render(lastHourAverageTemperature, lastDayAverageTemperature, lastWeekAverageTemperature));
+        Double lastHourAveragePressure = getLastHourAveragePressure();
+        Double lastDayAveragePressure = getLastDayAveragePressure();
+        Double lastWeekAveragePressure = getLastWeekAveragePressure();
+
+        return ok(index.render(lastHourAverageTemperature, lastDayAverageTemperature, lastWeekAverageTemperature, lastHourAverageHumidity, lastDayAverageHumidity, lastWeekAverageHumidity, lastHourAveragePressure, lastDayAveragePressure, lastWeekAveragePressure));
     }
 }
